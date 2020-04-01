88ce19bc5406de4b206378ed706f89d6d33855dc
ingles-autodidata <ingles.autodidata.lh@gmail.com>
2020-04-01 17:48:43 -0300

    Add start and finish points

--- src/Node.js
+++ src/Node.js
@@ -2,5 +2,15 @@ import React from 'react';
 import './Node.css';
 
 export default function Node(props) {
-  return <div id={`node-${props.row}-${props.col}`} className={'node'}></div>;
+  const extraClassName = props.isFinish
+    ? 'node-finish'
+    : props.isStart
+    ? 'node-start'
+    : '';
+  return (
+    <div
+      id={`node-${props.row}-${props.col}`}
+      className={`node ${extraClassName}`}
+    ></div>
+  );
 }
--- src/PathFindingVisualizer.js
+++ src/PathFindingVisualizer.js
@@ -1,22 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import Node from './Node';
 
+const START_NODE_ROW = 5;
+const START_NODE_COL = 15;
+const FINISH_NODE_ROW = 10;
+const FINISH_NODE_COL = 35;
+
 export default function PathFindingVisualizer() {
   const [grid, setGrid] = useState([]);
 
   useEffect(() => {
-    let n = [];
-    for (let row = 0; row < 20; row++) {
-      let currentRow = [];
-      for (let col = 0; col < 50; col++) {
-        currentRow.push([]);
-      }
-      n.push(currentRow);
-    }
+    const n = getInitialGrid();
     setGrid(n);
   }, []);
 
-  console.log(grid);
+  const createNode = (col, row) => {
+    return {
+      col,
+      row,
+      isStart: row === START_NODE_ROW && col === START_NODE_COL,
+      isFinish: row === FINISH_NODE_ROW && col === FINISH_NODE_COL
+    };
+  };
+
+  const getInitialGrid = () => {
+    const grid = [];
+    for (let row = 0; row < 20; row++) {
+      const currentRow = [];
+      for (let col = 0; col < 50; col++) {
+        currentRow.push(createNode(col, row));
+      }
+      grid.push(currentRow);
+    }
+    return grid;
+  };
+
   return (
     <div>
       <h1>Path Finding</h1>
@@ -25,9 +43,15 @@ export default function PathFindingVisualizer() {
           return (
             <div key={rowIdx}>
               {row.map((node, nodeIdx) => {
-                const { row, col } = node;
+                const { row, col, isStart, isFinish } = node;
                 return (
-                  <Node key={`${rowIdx}-${nodeIdx}`} col={col} row={row}></Node>
+                  <Node
+                    key={`${rowIdx}-${nodeIdx}`}
+                    col={col}
+                    row={row}
+                    isStart={isStart}
+                    isFinish={isFinish}
+                  ></Node>
                 );
               })}
             </div>
