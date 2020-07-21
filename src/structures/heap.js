c14a375f7e56039fa4e051ebed67910b4578f30c
lyangHiga <lyang_higa@poli.ufrj.br>
2020-07-21 15:31:06 -0300

    Add Decrease Key

--- src/structures/heap.js
+++ src/structures/heap.js
@@ -8,6 +8,8 @@ class Node {
 class Heap {
   constructor() {
     this.values = [];
+    // dict key to array idx => you say the key it returns the idx
+    this.idxs = {};
   }
 
   // return true if the element from index i is less than k idx element
@@ -28,6 +30,8 @@ class Heap {
     this.values.push(node);
     // last position to insert this new node
     let idx = this.values.length - 1;
+    // add idx of this key
+    this.idxs[key] = idx;
     // parent of this new node
     let parentIdx = Math.floor((idx - 1) / 2);
     // sort (while this new node is smaller than its parent)
@@ -36,6 +40,48 @@ class Heap {
       [this.values[idx], this.values[parentIdx]] = [
         this.values[parentIdx],
         this.values[idx],
+      ];
+
+      // swap idxs elements in dict key to idx
+      [
+        this.idxs[this.values[idx].key],
+        this.idxs[this.values[parentIdx].key],
+      ] = [
+        this.idxs[this.values[parentIdx].key],
+        this.idxs[this.values[idx].key],
+      ];
+      //   recalculate node, parent idx position
+      idx = parentIdx;
+      parentIdx = Math.floor((idx - 1) / 2);
+    }
+    return this;
+  }
+
+  //   update node and return this or return false
+  decrease(key, newVal) {
+    //   check if there is a value for this key, in other words
+    // check whether this key belongs to this heap
+    if (this.values[this.idxs[key]] === undefined) return false;
+    //   get idx of this key
+    let idx = this.idxs[key];
+    //   update node with new val
+    this.values[idx].val = newVal;
+    let parentIdx = Math.floor((idx - 1) / 2);
+    if (parentIdx < 0) return this;
+    while (this.lessThan(idx, parentIdx)) {
+      //swap
+      [this.values[idx], this.values[parentIdx]] = [
+        this.values[parentIdx],
+        this.values[idx],
+      ];
+
+      // swap idxs elements in dict key to idx
+      [
+        this.idxs[this.values[idx].key],
+        this.idxs[this.values[parentIdx].key],
+      ] = [
+        this.idxs[this.values[parentIdx].key],
+        this.idxs[this.values[idx].key],
       ];
       //   recalculate node, parent idx position
       idx = parentIdx;
@@ -74,6 +120,16 @@ class Heap {
         this.values[smallIdx],
         this.values[idx],
       ];
+
+      // swap idxs elements in dict key to idx
+      [
+        this.idxs[this.values[idx].key],
+        this.idxs[this.values[smallIdx].key],
+      ] = [
+        this.idxs[this.values[smallIdx].key],
+        this.idxs[this.values[idx].key],
+      ];
+
       // update idx and its children
       idx = smallIdx;
       lChild = 2 * idx + 1;
