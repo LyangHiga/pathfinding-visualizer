e1db258da9ba5e58714bf3a5888390df21b3842c
lyanghiga <lyang_higa@poli.ufrj.br>
2021-02-19 11:22:24 -0300

    Add Bellman-Ford test for start and target in different CC, all nodes same positive weight

--- src/__tests__/algorithms/bellmanFord.test.ts
+++ src/__tests__/algorithms/bellmanFord.test.ts
@@ -1,9 +1,14 @@
 import Grid from "../../models/Grid";
 import bellmanFord from "../../algorithms/bellmanFord";
-import { valToIndx } from "../../helpers/gridHelper";
+import { valToIndx, toggleIsWall } from "../../helpers/gridHelper";
 
 const START = 10;
 const TARGET = 14;
+const U = 2;
+const V = 7;
+const X = 12;
+const Y = 17;
+const Z = 22;
 
 const NCOLS = 5;
 const NROWS = 5;
@@ -58,4 +63,42 @@ test("Bellman-Ford: Test Path when all nodes have the same positive weight", asy
   expect(path![0]).toBe(11);
   expect(path![1]).toBe(12);
   expect(path![2]).toBe(13);
+});
+
+//       0  1   2   3   4      0   1   X   3   4
+//       5  6   7   8   9      5   6   X   8   9
+// g =   S  11  12  13  T  =>  S   11  X   13  T
+//      15  16  17  18  19     15  16  X   18  19
+//      20  21  22  23  24     20  21  X   23  24
+test("Bellman-Ford: start and target nodes in different connected component, all nodes with the same positive weight ", async () => {
+  // weight is in [min,max) ,iow all weights are 1
+  const min = 1;
+  const max = 2;
+  const g = new Grid(START, TARGET, NROWS, NCOLS, max, min);
+
+  // get row and col of start and target
+  const [startRow, startCol] = valToIndx(START, NCOLS);
+  const [targetRow, targetCol] = valToIndx(TARGET, NCOLS);
+
+  // make nodes: 2,7,12,17,22 walls
+  const [uRow, uCol] = valToIndx(U, NCOLS);
+  const [vRow, vCol] = valToIndx(V, NCOLS);
+  const [xRow, xCol] = valToIndx(X, NCOLS);
+  const [yRow, yCol] = valToIndx(Y, NCOLS);
+  const [zRow, zCol] = valToIndx(Z, NCOLS);
+
+  toggleIsWall(g.grid[uRow][uCol]);
+  toggleIsWall(g.grid[vRow][vCol]);
+  toggleIsWall(g.grid[xRow][xCol]);
+  toggleIsWall(g.grid[yRow][yCol]);
+  toggleIsWall(g.grid[zRow][zCol]);
+  const { cycle, path, parents } = await bellmanFord(
+    g,
+    g.grid[startRow][startCol],
+    g.grid[targetRow][targetCol],
+    TEST
+  );
+  expect(cycle).toBeFalsy();
+  expect(path).toBeNull();
+  expect(parents.get(TARGET)).toBeUndefined();
 });
