c3b5139d3314c60943bd9c223d8fee0990fa91f2
lyanghiga <lyang_higa@poli.ufrj.br>
2021-03-01 16:42:23 -0300

    Test for an unweighted grid for any number of rows and cols

--- src/__tests__/components/grid.test.tsx
+++ src/__tests__/components/grid.test.tsx
@@ -1,5 +1,3 @@
-// TODO: test with different g sizes, weighted or not
-
 import React from "react";
 import { shallow } from "enzyme";
 import Grid from "../../components/Grid";
@@ -51,4 +49,23 @@ it("Render a Grid with RANDOM_ROWS and RANDOM_COLS", () => {
   expect(wrapper.props().children.length).toBe(RANDOM_ROWS);
   // Check if Grid has (<RANDOM_COLS>) rows
   expect(wrapper.props().children[0].props.children.length).toBe(RANDOM_COLS);
+});
+
+it("Render an unweighted Grid", () => {
+  const RANDOM_ROWS = Math.floor(Math.random() * 10);
+  const RANDOM_COLS = Math.floor(Math.random() * 20);
+  const g = new GridModel(START, TARGET, RANDOM_ROWS, RANDOM_COLS);
+  const wrapper = shallow(
+    <Grid
+      grid={g}
+      isWeighted={false}
+      handleMouseDown={jest.fn()}
+      handleMouseEnter={jest.fn()}
+      handleMouseUp={jest.fn()}
+    />
+  );
+  expect(wrapper.props().children.length).toBe(RANDOM_ROWS);
+  expect(wrapper.props().children[0].props.children.length).toBe(RANDOM_COLS);
+  // check if Grid is unweighted
+  expect(wrapper.props().children[0].props.children[0].props.isWeighted);
 });
